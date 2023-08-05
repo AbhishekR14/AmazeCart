e7b4de7c85728621a683e215f8edb456641048e1
AbhishekR14 <aranjan0288@gmail.com>
2023-08-05 12:10:35 +0530

    implementing the delete button in cart

--- backendData/cart.js
+++ backendData/cart.js
@@ -1,7 +1,17 @@
-export const cart = [{
+export let cart = [{
     id: "dhbd55151-sub52136-wiuib845-apn-5326gdv",
     quantity: 2
 },{
     id: "qpwon2103-mnbj5646-jhsfvj84-sgvfsh42",
     quantity: 2
 }];
+
+export function deleteItemFromCart(id){
+    let newCart =[];
+    cart.forEach((cartItem => {
+        if (cartItem.id !== id){
+            newCart.push(cartItem);
+        }
+    }))
+    cart = newCart;
+};
